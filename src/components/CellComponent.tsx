b0d73e005b0e3fcdeb645cd97f03164e082ab272
Roman15122022 <120688434+Roman15122022@users.noreply.github.com>
2024-01-03 00:57:00 +0200

    feat: todo

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from 'react';
 import {Cell} from "../models/Cell";
 import {Simulate} from "react-dom/test-utils";
-import click = Simulate.click;
+
 
 interface CellProps {
     cell: Cell;
@@ -11,9 +11,11 @@ interface CellProps {
 
 const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
     return (
-        <div className={['cell', cell.color, selected ? 'selected': ''].join(' ')}
-            onClick={() => click(cell)}
+        <div className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
+             onClick={() => click(cell)}
+             style={{background: cell.available && !cell.figure ? 'green' : ''}}
         >
+            {cell.available && !cell.figure && <div className='available'/>}
             {cell.figure?.logo && <img src={cell.figure.logo} alt="figure"/>}
         </div>
     );
