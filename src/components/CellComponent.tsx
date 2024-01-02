b6ef583836bc2e8ae297d8894562238a134642ef
Roman15122022 <120688434+Roman15122022@users.noreply.github.com>
2024-01-03 00:32:33 +0200

    fix: add Figures and click event

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,13 +1,19 @@
 import React, {FC} from 'react';
 import {Cell} from "../models/Cell";
+import {Simulate} from "react-dom/test-utils";
+import click = Simulate.click;
 
 interface CellProps {
-    cell: Cell
+    cell: Cell;
+    selected: boolean;
+    click: (cell: Cell) => void
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
     return (
-        <div className={['cell', cell.color].join(' ')}>
+        <div className={['cell', cell.color, selected ? 'selected': ''].join(' ')}
+            onClick={() => click(cell)}
+        >
             {cell.figure?.logo && <img src={cell.figure.logo} alt="figure"/>}
         </div>
     );
